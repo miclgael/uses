e21d85df1d5a6605371f2a9ecc66067fd48c424e
Michael Gale <michael@michaelgale.dev>
2020-01-22 17:55:16 +1100

    idea for static site / page

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const showdown = require('showdown')
+const converter = new showdown.Converter()
+
+// INPUT
+const blob = fs.readFileSync('README.md', 'utf8')
+
+// PROCESS
+const html = converter.makeHtml(blob)
+
+// OUTPUT
+fs.writeFileSync('index.html', html)
